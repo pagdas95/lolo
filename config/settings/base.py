76f328f28416fb8092f18bf2ee2528b7884859ac
pagdas95 <pantelisa1995@gmail.com>
2024-11-08 22:32:06 +0200

    remove the / from the cors

--- config/settings/base.py
+++ config/settings/base.py
@@ -370,7 +370,7 @@ REST_AUTH = {
 CORS_URLS_REGEX = r"^/api/.*$"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Adjust based on where the frontend is hosted
-    "https://lolo-sable.vercel.app/"
+    "https://lolo-sable.vercel.app"
 ]
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
