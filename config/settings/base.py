44709b4ad53cd99692b79a4d08e34d3dba9d00a1
pagdas95 <pantelisa1995@gmail.com>
2024-11-08 22:11:13 +0200

    corsor

--- config/settings/base.py
+++ config/settings/base.py
@@ -368,7 +368,10 @@ REST_AUTH = {
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
 CORS_URLS_REGEX = r"^/api/.*$"
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # Adjust based on where the frontend is hosted
+    "https://lolo-sable.vercel.app/"
+]
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
 SPECTACULAR_SETTINGS = {
