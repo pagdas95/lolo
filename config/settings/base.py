5151755bd462f5c5f7ea4f8d7b87709f588ee2a1
pagdas95 <pantelisa1995@gmail.com>
2024-12-18 02:04:19 +0200

    add versel on cors

--- config/settings/base.py
+++ config/settings/base.py
@@ -383,6 +383,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://127.0.0.1:3000",
     "*laughterolympics-uat*",
+    "https://vercel.com",
     "https://laughterolympics.com",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
